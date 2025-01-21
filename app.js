7209fe033df9056eaef3955ac4f76510dc625143
lautaroff <lauferreyraff@gmail.com>
2025-01-21 18:42:38 -0300

    add: js funcionalities - final version

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let lista_amigos = [];
+function agregarAmigo() {
+    let amigo =document.getElementById("amigo").value;
+    if (amigo == " "){
+        alert("Ingrese un nombre valido")
+        return;
+    }else if (lista_amigos.includes(amigo)){
+        alert("El amigo ya se encuentra en la lista")
+    }else{
+        lista_amigos.push(amigo);
+    }  
+    mostrarAmigos(); 
+    limpiarCampo();
+}
+function mostrarAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < lista_amigos.length; i++){
+        lista.innerHTML += `<li>${lista_amigos[i]}</li>`;
+    }
+}
+function limpiarCampo(){
+    document.getElementById("amigo").value = "";
+}
+
+
+function sortearAmigo(){
+    let amigo = lista_amigos[Math.floor(Math.random() * lista_amigos.length)];
+    document.getElementById("resultado").innerHTML = amigo;
+}
